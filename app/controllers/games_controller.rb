f94565390220898f1a11578b57f69aaa4c46c095
Scott Ha <123viet123@gmail.com>
2020-02-17 11:33:07 -0800

    Moved from SQLite3 to PostGreSQL

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,19 @@
 class GamesController < ApplicationController
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      flash[:notice] = "Successfully saved game"
+    else
+      flash[:errors] = @game.errors.full_messages.join('');
+    else
+      
+      redirect_to users_url
+    end
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:date_played, :white_id, :black_id, :winning_color, :win_by_mate)
+  end
 end
